6ec807ac32db33badc70e4b4c905cdb797fed363
alban-okoby <albanokoby225@gmail.com>
2024-05-01 18:14:48 +0000

    React Redux action creator+normalizr

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Given an n x n 2D matrix, rotate it 90 degrees clockwise"""
+
+
+def rotate_2d_matrix(matrix):
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    
+    for i in range(n):
+        matrix[i].reverse()
+
+if __name__ == "__main__":
+    matrix = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]
+
+    rotate_2d_matrix(matrix)
+    print(matrix)
